18888e5dfe7527aab117326e7460cab702975d78
aryanrods <rodriguesaryan00@gmai.com>
2025-03-13 13:16:30 +1100

    constructor set

--- Car.java
+++ Car.java
@@ -6,6 +6,44 @@ public class Car {
     private String model;
     private float price;
 
+    public Car(String numplate) {
+        this.numplate = numplate;
+    }
+
+    public Car(String numplate, String make, String model, float price) {
+        this.numplate = numplate;
+        this.make = make;
+        this.model = model;
+        this.price = price;
+    }
+
+    public Car() {
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
     public String getNumPlate() {
         return numplate;
     }
--- Main.java
+++ Main.java
@@ -8,10 +8,10 @@ public class Main {
 
         Scanner console = new Scanner(System.in);
 
-        Car car = new Car();
         String UserInput;
         System.out.println("Enter the number plate of the car: ");
         UserInput = console.next();
+        Car car = new Car(UserInput, "Toyato", "Fortuner", 1000000);
 
         car.setNumPlate(UserInput);
         System.out.println("Number plate set as :  " + car.getNumPlate());
