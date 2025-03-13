c210a322587d3d3346f2cb29b62b927f23133ff7
aryanrods <rodriguesaryan00@gmai.com>
2025-03-13 12:57:04 +1100

    input as number plate

--- Main.java
+++ Main.java
@@ -6,12 +6,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Get Input from the User");
 
-        Scanner Input = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
 
         Car car = new Car();
+        String UserInput;
+        System.out.println("Enter the number plate of the car: ");
+        UserInput = console.next();
 
-        car.setNumPlate("ABC123");
-        car.getNumPlate();
+        car.setNumPlate(UserInput);
+        System.out.println("Number plate set as :  " + car.getNumPlate());
 
     }
 }
